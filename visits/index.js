d581f14656c7b9c4eac56605eeefc6306b6976b6
imakash3011 <imakashpatel3011@gmail.com>
2021-01-31 15:34:52 +0530

    add dockerfile and docker-compose file

--- visits/index.js
+++ visits/index.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const redis = require('redis')
+const process = require('process')
 
 const app = express()
-const client = redis.createClient();
+const client = redis.createClient({
+    // host will help to connect to the redis-server from docker-compose
+    host: 'redis-server',
+    port : 6379
+});
 client.set('visits',0)
 
 app.get('/',(req,res)=>{
+    // forcing sysytem to get crash
+    process.exit(0);
     client.get('visits',(err,visits)=>{
         res.send('Number of visits is '+ visits);
         client.set('visits', parseInt(visits)+1);
@@ -13,5 +20,5 @@ app.get('/',(req,res)=>{
 })
 
 app.listen(8081,()=>{
-    console.log('Lsitening on port 8081')
+    console.log('Listening on port 8081')
 })
